bfa23d03d146371386b2c5af7bd9b4e0a937f436
tdakkota <tanc13@yandex.ru>
2021-03-25 23:19:38 +0300

    fix(bot): commit state even if bot is blocked by user

--- bot.go
+++ bot.go
@@ -228,11 +228,11 @@ func (b *Bot) Handle(ctx context.Context, e tg.Entities, msg tg.MessageClass) er
 
 func (b *Bot) OnNewMessage(ctx context.Context, e tg.Entities, u *tg.UpdateNewMessage) error {
 	if err := b.Handle(ctx, e, u.Message); err != nil {
-		if tg.IsUserBlocked(err) {
-			b.logger.Debug("Bot is blocked by user")
-			return nil
+		if !tg.IsUserBlocked(err) {
+			return xerrors.Errorf("handle message %d: %w", u.Message.GetID(), err)
 		}
-		return xerrors.Errorf("handle message %d: %w", u.Message.GetID(), err)
+
+		b.logger.Debug("Bot is blocked by user")
 	}
 
 	if err := b.state.Commit(u.Pts); err != nil {
