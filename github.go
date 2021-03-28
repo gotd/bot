1f544c53fe0727ae316e7c0b9302386369535d02
Ivan Trubach <mr.trubach@icloud.com>
2021-03-28 23:29:23 +0300

    fix(github): get installation token for app

--- github.go
+++ github.go
@@ -5,11 +5,19 @@ import (
 	"net/http/httputil"
 
 	"github.com/google/go-github/v33/github"
+	"golang.org/x/oauth2"
 	"golang.org/x/xerrors"
 
 	"github.com/gotd/td/telegram/message"
 	"github.com/gotd/td/telegram/message/styling"
 	"github.com/gotd/td/tg"
+)
+
+const (
+	githubOwner    = "gotd"
+	githubRepo     = "td"
+	githubRef      = "main"
+	githubWorkflow = "bot.yml"
 )
 
 func (b *Bot) answerGH(
@@ -24,10 +32,29 @@ func (b *Bot) answerGH(
 			return xerrors.Errorf("send: %w", err)
 		}
 	}
+
+	// Create client with short-lived repository installation token.
+	//
+	inst, _, err := gh.Apps.FindRepositoryInstallation(ctx, githubOwner, githubRepo)
+	if err != nil {
+		return xerrors.Errorf("find repository installation: %w", err)
+	}
+	tok, _, err := gh.Apps.CreateInstallationToken(ctx, inst.GetID(), nil)
+	if err != nil {
+		return xerrors.Errorf("create installation token: %w", err)
+	}
+	gh = github.NewClient(oauth2.NewClient(ctx, oauth2.StaticTokenSource(
+		&oauth2.Token{
+			AccessToken: tok.GetToken(),
+		},
+	)))
+
+	// Reply with response header.
+
 	resp, err := gh.Actions.CreateWorkflowDispatchEventByFileName(
-		ctx, "gotd", "td", "bot.yml",
+		ctx, githubOwner, githubRepo, githubWorkflow,
 		github.CreateWorkflowDispatchEventRequest{
-			Ref: "main",
+			Ref: githubRef,
 			Inputs: map[string]interface{}{
 				"telegram": true,
 				"message":  m,
@@ -37,12 +64,10 @@ func (b *Bot) answerGH(
 	if err != nil {
 		return xerrors.Errorf("dispatch workflow: %w", err)
 	}
-
 	data, err := httputil.DumpResponse(resp.Response, true)
 	if err != nil {
 		return xerrors.Errorf("dump response: %w", err)
 	}
-
 	if _, err := send.StyledText(ctx, styling.Pre(string(data), "")); err != nil {
 		return xerrors.Errorf("send: %w", err)
 	}
