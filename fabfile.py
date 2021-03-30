82ef37c89031383c138fdda1bdd48cfe619fd178
Aleksandr Razumov <ernado@ya.ru>
2021-03-30 22:19:59 +0300

    chore: add fabfile

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric import task, Connection
+
+
+@task
+def deploy(c):
+    """
+    Deploy bot to production
+
+    :type c: Connection
+    """
+    c.put('./bot', '/tmp/bot')
+    c.run('mv /tmp/bot /home/bot/bot')
+
+    c.run('systemctl restart bot')
+
