b9d0f2f9da5a73dc8da6e27c01788b87325bd84a
Aleksandr Razumov <ernado@ya.ru>
2021-04-13 20:45:41 +0300

    refactor(github): move methods

--- github.go
+++ github.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"sync"
+	"time"
 
+	"github.com/andres-erbsen/clock"
 	"github.com/google/go-github/v33/github"
 	"golang.org/x/oauth2"
 	"golang.org/x/xerrors"
 
 	"github.com/gotd/td/telegram/message"
+	"github.com/gotd/td/telegram/message/peer"
 	"github.com/gotd/td/telegram/message/styling"
 	"github.com/gotd/td/tg"
 )
@@ -82,4 +87,123 @@ func (b *Bot) answerGH(
 
 		return nil
 	})
+}
+
+type resolved struct {
+	time time.Time
+	peer tg.InputPeerClass
+}
+
+type ttlResolver struct {
+	clock    clock.Clock
+	resolver peer.Resolver
+	duration time.Duration
+	mux      sync.Mutex
+	data     map[string]resolved
+}
+
+func (r *ttlResolver) ResolvePhone(ctx context.Context, phone string) (tg.InputPeerClass, error) {
+	return r.resolver.ResolvePhone(ctx, phone)
+}
+
+func (r *ttlResolver) ResolveDomain(ctx context.Context, s string) (tg.InputPeerClass, error) {
+	r.mux.Lock()
+	defer r.mux.Unlock()
+
+	x, ok := r.data[s]
+	if ok && r.clock.Now().Sub(x.time) < r.duration {
+		return x.peer, nil
+	}
+
+	p, err := r.resolver.ResolveDomain(ctx, s)
+	if err != nil {
+		return nil, err
+	}
+
+	x.peer = p
+	x.time = r.clock.Now()
+	r.data[s] = x
+
+	return p, nil
+}
+
+func getPullRequestURL(e *github.PullRequestEvent) styling.StyledTextOption {
+	urlName := fmt.Sprintf("%s#%d",
+		e.GetRepo().GetFullName(),
+		e.PullRequest.GetNumber(),
+	)
+
+	return styling.TextURL(urlName, e.GetPullRequest().GetHTMLURL())
+}
+
+func (b *Bot) handlePRClosed(ctx context.Context, e *github.PullRequestEvent) error {
+	if !e.GetPullRequest().GetMerged() {
+		b.logger.Info("Ignoring non-merged PR")
+		return nil
+	}
+
+	p, err := b.resolver.ResolveDomain(ctx, b.notifyGroup)
+	if err != nil {
+		return xerrors.Errorf("resolve: %w", err)
+	}
+
+	if _, err := b.sender.To(p).StyledText(ctx,
+		styling.Plain("Pull request "),
+		getPullRequestURL(e),
+		styling.Plain(" merged:\n\n"),
+		styling.Italic(e.GetPullRequest().GetTitle()),
+	); err != nil {
+		return xerrors.Errorf("send: %w", err)
+	}
+
+	return nil
+}
+
+func (b *Bot) handlePROpened(ctx context.Context, e *github.PullRequestEvent) error {
+	p, err := b.resolver.ResolveDomain(ctx, b.notifyGroup)
+	if err != nil {
+		return xerrors.Errorf("resolve: %w", err)
+	}
+
+	if _, err := b.sender.To(p).StyledText(ctx,
+		styling.Plain("New pull request "),
+		getPullRequestURL(e),
+		styling.Plain(" opened:\n\n"),
+		styling.Italic(e.GetPullRequest().GetTitle()),
+	); err != nil {
+		return xerrors.Errorf("send: %w", err)
+	}
+
+	return nil
+}
+
+func (b *Bot) handlePR(ctx context.Context, e *github.PullRequestEvent) error {
+	switch e.GetAction() {
+	case "opened":
+		return b.handlePROpened(ctx, e)
+	case "closed":
+		return b.handlePRClosed(ctx, e)
+	}
+	return nil
+}
+
+func (b *Bot) handleRelease(ctx context.Context, e *github.ReleaseEvent) error {
+	if e.GetAction() != "published" {
+		return nil
+	}
+
+	p, err := b.resolver.ResolveDomain(ctx, b.notifyGroup)
+	if err != nil {
+		return xerrors.Errorf("resolve: %w", err)
+	}
+
+	if _, err := b.sender.To(p).StyledText(ctx,
+		styling.Plain("New release: "),
+		styling.TextURL(e.GetRelease().GetTagName(), e.GetRelease().GetHTMLURL()),
+		styling.Plain(fmt.Sprintf(" for %s", e.GetRepo().GetFullName())),
+	); err != nil {
+		return xerrors.Errorf("send: %w", err)
+	}
+
+	return nil
 }
--- http.go
+++ http.go
@@ -1,21 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"sync"
-	"time"
 
 	"github.com/google/go-github/v33/github"
 	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
-	"golang.org/x/xerrors"
-
-	"github.com/gotd/td/clock"
-	"github.com/gotd/td/telegram/message/peer"
-	"github.com/gotd/td/telegram/message/styling"
-	"github.com/gotd/td/tg"
 )
 
 func (b *Bot) RegisterRoutes(e *echo.Echo) {
@@ -59,123 +50,4 @@ func (b *Bot) handleHook(e echo.Context) error {
 		log.Info("No handler")
 		return e.String(http.StatusOK, "ok")
 	}
-}
-
-type resolved struct {
-	time time.Time
-	peer tg.InputPeerClass
-}
-
-type ttlResolver struct {
-	clock    clock.Clock
-	resolver peer.Resolver
-	duration time.Duration
-	mux      sync.Mutex
-	data     map[string]resolved
-}
-
-func (r *ttlResolver) ResolvePhone(ctx context.Context, phone string) (tg.InputPeerClass, error) {
-	return r.resolver.ResolvePhone(ctx, phone)
-}
-
-func (r *ttlResolver) ResolveDomain(ctx context.Context, s string) (tg.InputPeerClass, error) {
-	r.mux.Lock()
-	defer r.mux.Unlock()
-
-	x, ok := r.data[s]
-	if ok && r.clock.Now().Sub(x.time) < r.duration {
-		return x.peer, nil
-	}
-
-	p, err := r.resolver.ResolveDomain(ctx, s)
-	if err != nil {
-		return nil, err
-	}
-
-	x.peer = p
-	x.time = r.clock.Now()
-	r.data[s] = x
-
-	return p, nil
-}
-
-func getPullRequestURL(e *github.PullRequestEvent) styling.StyledTextOption {
-	urlName := fmt.Sprintf("%s#%d",
-		e.GetRepo().GetFullName(),
-		e.PullRequest.GetNumber(),
-	)
-
-	return styling.TextURL(urlName, e.GetPullRequest().GetHTMLURL())
-}
-
-func (b *Bot) handlePRClosed(ctx context.Context, e *github.PullRequestEvent) error {
-	if !e.GetPullRequest().GetMerged() {
-		b.logger.Info("Ignoring non-merged PR")
-		return nil
-	}
-
-	p, err := b.resolver.ResolveDomain(ctx, b.notifyGroup)
-	if err != nil {
-		return xerrors.Errorf("resolve: %w", err)
-	}
-
-	if _, err := b.sender.To(p).StyledText(ctx,
-		styling.Plain("Pull request "),
-		getPullRequestURL(e),
-		styling.Plain(" merged:\n\n"),
-		styling.Italic(e.GetPullRequest().GetTitle()),
-	); err != nil {
-		return xerrors.Errorf("send: %w", err)
-	}
-
-	return nil
-}
-
-func (b *Bot) handlePROpened(ctx context.Context, e *github.PullRequestEvent) error {
-	p, err := b.resolver.ResolveDomain(ctx, b.notifyGroup)
-	if err != nil {
-		return xerrors.Errorf("resolve: %w", err)
-	}
-
-	if _, err := b.sender.To(p).StyledText(ctx,
-		styling.Plain("New pull request "),
-		getPullRequestURL(e),
-		styling.Plain(" opened:\n\n"),
-		styling.Italic(e.GetPullRequest().GetTitle()),
-	); err != nil {
-		return xerrors.Errorf("send: %w", err)
-	}
-
-	return nil
-}
-
-func (b *Bot) handlePR(ctx context.Context, e *github.PullRequestEvent) error {
-	switch e.GetAction() {
-	case "opened":
-		return b.handlePROpened(ctx, e)
-	case "closed":
-		return b.handlePRClosed(ctx, e)
-	}
-	return nil
-}
-
-func (b *Bot) handleRelease(ctx context.Context, e *github.ReleaseEvent) error {
-	if e.GetAction() != "published" {
-		return nil
-	}
-
-	p, err := b.resolver.ResolveDomain(ctx, b.notifyGroup)
-	if err != nil {
-		return xerrors.Errorf("resolve: %w", err)
-	}
-
-	if _, err := b.sender.To(p).StyledText(ctx,
-		styling.Plain("New release: "),
-		styling.TextURL(e.GetRelease().GetTagName(), e.GetRelease().GetHTMLURL()),
-		styling.Plain(fmt.Sprintf(" for %s", e.GetRepo().GetFullName())),
-	); err != nil {
-		return xerrors.Errorf("send: %w", err)
-	}
-
-	return nil
 }
