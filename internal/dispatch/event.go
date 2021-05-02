89a5fea95fbc53280716bfc0f34985136a729f26
tdakkota <tanc13@yandex.ru>
2021-05-02 23:07:31 +0300

    feat(dispatch): add WithReply support for chats and users

--- internal/dispatch/event.go
+++ internal/dispatch/event.go
@@ -58,6 +58,34 @@ func (e MessageEvent) Sender() *message.Sender {
 	return e.sender
 }
 
+func findMessage(r tg.MessagesMessagesClass, msgID int) (*tg.Message, error) {
+	slice, ok := r.(interface{ GetMessages() []tg.MessageClass })
+	if !ok {
+		return nil, xerrors.Errorf("unexpected type %T", r)
+	}
+
+	msgs := slice.GetMessages()
+	for _, m := range msgs {
+		msg, ok := m.(*tg.Message)
+		if !ok || msg.ID != msgID {
+			continue
+		}
+
+		return msg, nil
+	}
+
+	return nil, xerrors.Errorf("message %d not found in response %+v", msgID, msgs)
+}
+
+func (e MessageEvent) getMessage(ctx context.Context, msgID int) (*tg.Message, error) {
+	r, err := e.rpc.MessagesGetMessages(ctx, []tg.InputMessageClass{&tg.InputMessageID{ID: msgID}})
+	if err != nil {
+		return nil, xerrors.Errorf("get message: %w", err)
+	}
+
+	return findMessage(r, msgID)
+}
+
 func (e MessageEvent) getChannelMessage(ctx context.Context, channel *tg.InputChannel, msgID int) (*tg.Message, error) {
 	r, err := e.rpc.ChannelsGetMessages(ctx, &tg.ChannelsGetMessagesRequest{
 		Channel: channel,
@@ -67,22 +95,7 @@ func (e MessageEvent) getChannelMessage(ctx context.Context, channel *tg.InputCh
 		return nil, xerrors.Errorf("get message: %w", err)
 	}
 
-	slice, ok := r.(interface{ GetMessages() []tg.MessageClass })
-	if !ok {
-		return nil, xerrors.Errorf("unexpected type %T", r)
-	}
-
-	msgs := slice.GetMessages()
-	if len(msgs) < 1 {
-		return nil, xerrors.Errorf("unexpected empty response %+v", msgs)
-	}
-
-	msg, ok := msgs[0].(*tg.Message)
-	if !ok {
-		return nil, xerrors.Errorf("unexpected type %T", msg)
-	}
-
-	return msg, nil
+	return findMessage(r, msgID)
 }
 
 // WithReply calls given callback if current message event is a reply message.
@@ -95,22 +108,31 @@ func (e MessageEvent) WithReply(ctx context.Context, cb func(reply *tg.Message) 
 		return nil
 	}
 
-	channel, ok := e.Peer.(*tg.InputPeerChannel)
-	if !ok {
-		// Skip non-channel messages.
-		return nil
-	}
-
-	e.logger.Info("Fetching message",
-		zap.Int("msg_id", e.Message.ID),
-		zap.Int("reply_to_msg_id", h.ReplyToMsgID),
-		zap.Int("channel_id", channel.ChannelID),
+	var (
+		msg *tg.Message
+		err error
+		log = e.logger.With(
+			zap.Int("msg_id", e.Message.ID),
+			zap.Int("reply_to_msg_id", h.ReplyToMsgID),
+		)
 	)
+	switch p := e.Peer.(type) {
+	case *tg.InputPeerChannel:
+		log.Info("Fetching message", zap.Int("channel_id", p.ChannelID))
 
-	msg, err := e.getChannelMessage(ctx, &tg.InputChannel{
-		ChannelID:  channel.ChannelID,
-		AccessHash: channel.AccessHash,
-	}, h.ReplyToMsgID)
+		msg, err = e.getChannelMessage(ctx, &tg.InputChannel{
+			ChannelID:  p.ChannelID,
+			AccessHash: p.AccessHash,
+		}, h.ReplyToMsgID)
+	case *tg.InputPeerChat:
+		log.Info("Fetching message", zap.Int("chat_id", p.ChatID))
+
+		msg, err = e.getMessage(ctx, h.ReplyToMsgID)
+	case *tg.InputPeerUser:
+		log.Info("Fetching message", zap.Int("chat_id", p.UserID))
+
+		msg, err = e.getMessage(ctx, h.ReplyToMsgID)
+	}
 	if err != nil {
 		if _, err := e.Reply().Text(ctx, fmt.Sprintf("Message %d not found", h.ReplyToMsgID)); err != nil {
 			return xerrors.Errorf("send: %w", err)
