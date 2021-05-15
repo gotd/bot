c3f7a5603ac381af14f483154aa7e8a7855391b4
tdakkota <tanc13@yandex.ru>
2021-05-15 16:30:24 +0300

    feat(webhook): handle opened issue event

---
+++ internal/gh/handle_issue.go
@@ -0,0 +1,59 @@
+package gh
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/go-github/v33/github"
+	"golang.org/x/xerrors"
+
+	"github.com/gotd/td/telegram/message/entity"
+	"github.com/gotd/td/telegram/message/styling"
+)
+
+func (h Webhook) handleIssue(ctx context.Context, e *github.IssuesEvent) error {
+	if e.GetAction() != "opened" {
+		return nil
+	}
+
+	p, err := h.notifyPeer(ctx)
+	if err != nil {
+		return xerrors.Errorf("peer: %w", err)
+	}
+
+	issue := e.GetIssue()
+	sender := e.GetSender()
+	formatter := func(eb *entity.Builder) error {
+		eb.Plain("New issue ")
+		urlName := fmt.Sprintf("%s#%d",
+			e.GetRepo().GetFullName(),
+			issue.GetNumber(),
+		)
+		eb.TextURL(urlName, issue.GetHTMLURL())
+		eb.Plain(" by ")
+		eb.TextURL(sender.GetLogin(), sender.GetHTMLURL())
+		eb.Plain("\n\n")
+
+		eb.Italic(issue.GetTitle())
+		eb.Plain("\n\n")
+
+		eb.Italic("Labels:")
+		for _, label := range issue.Labels {
+			switch label.GetName() {
+			case "":
+				continue
+			case "bug":
+				eb.Bold(label.GetName())
+			default:
+				eb.Italic(label.GetName())
+			}
+		}
+		return nil
+	}
+
+	if _, err := h.sender.To(p).NoWebpage().
+		StyledText(ctx, styling.Custom(formatter)); err != nil {
+		return xerrors.Errorf("send: %w", err)
+	}
+	return nil
+}
