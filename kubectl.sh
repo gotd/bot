1f44579d2df836ed100a906442ee9e80c5787f32
Aleksandr Razumov <ernado@ya.ru>
2021-11-28 14:12:29 +0300

    ci: rework kubectl

---
+++ kubectl.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+version="$1"
+config="$2"
+if [ "$version" = "latest" ]; then
+  version=$(curl -Ls https://dl.k8s.io/release/stable.txt)
+fi
+
+echo "using kubectl@{$version}"
+
+curl -sLO "https://dl.k8s.io/release/${version}/bin/linux/amd64/kubectl" -o kubectl
+chmod +x kubectl
+
+# Extract the base64 encoded config data and write this to the KUBECONFIG
+echo "${config}" | base64 -d > /tmp/config
+export KUBECONFIG=/tmp/config
+
+sh -c "./kubectl ${command}"
