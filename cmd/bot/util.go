1f9ef4260f5200b5dacbf1d8b0811a8f7e88002d
Aleksandr Razumov <ernado@go-faster.org>
2024-12-02 10:34:48 +0300

    chore: rm unused func

--- cmd/bot/util.go
+++ cmd/bot/util.go
@@ -9,7 +9,3 @@ func tokHash(token string) string {
 	h := md5.Sum([]byte(token + "gotd-token-salt")) // #nosec
 	return fmt.Sprintf("%x", h[:5])
 }
-
-func sessionFileName(token string) string {
-	return fmt.Sprintf("bot.%s.session.json", tokHash(token))
-}
